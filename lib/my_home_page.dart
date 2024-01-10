ae9b9665d7c94e4584d0270f6d141a9df5650af3
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-01-10 14:18:35 +0530

    basic info showed

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_environment/common/app_config.dart';
 
@@ -11,6 +14,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  PackageInfo? packageInfo;
+
+  @override
+  void initState() {
+    SchedulerBinding.instance.addPersistentFrameCallback((timeStamp) async {
+      packageInfo = await PackageInfo.fromPlatform();
+      print('Running on ${packageInfo!.packageName}'); // e.g. "Moto G (4)"
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,20 +34,28 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text("Flutter Environment"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Selected Environment',
-              style: TextStyle(fontSize: 40),
-            ),
-            Text(
-              '${widget.appConfig.flavor}'
-                  '\n${widget.appConfig.baseUrl}'
-                  '\n${widget.appConfig.someValue}',
-              style: TextStyle(fontSize: 25),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Configuration: \n'
+                '${widget.appConfig.flavor}'
+                '\n${widget.appConfig.baseUrl}'
+                '\n${widget.appConfig.someValue}'
+                '\n${kReleaseMode ? "Release Mode" : "Debug Mode"}',
+                style: const TextStyle(fontSize: 25),
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              Text(
+                'Package name: \n'
+                '${packageInfo?.packageName ?? ""}',
+                style: const TextStyle(fontSize: 25),
+              ),
+            ],
+          ),
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
